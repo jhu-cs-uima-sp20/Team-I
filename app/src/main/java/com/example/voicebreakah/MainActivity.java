45d8f10113fb22164e52a13dcbbbb83009892e64
Stanley Zheng <szheng22@jhu.edu>
2020-04-07 00:57:01 -0400

    added more buttons

--- app/src/main/java/com/example/voicebreakah/MainActivity.java
+++ app/src/main/java/com/example/voicebreakah/MainActivity.java
@@ -14,10 +14,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button startGameButton = findViewById(R.id.start_game);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button settingsButton = findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+        Button skinsButton = findViewById(R.id.skins_button);
+        skinsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SkinsActivity.class);
                 startActivity(intent);
             }
         });
