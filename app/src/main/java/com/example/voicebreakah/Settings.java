39e0749e3a30796b5a9830be42514714b23dca60
Charissa Zou <charissa.zou@gmail.com>
2020-04-06 23:27:25 -0500

    Finished settings layout

--- app/src/main/java/com/example/voicebreakah/Settings.java
+++ app/src/main/java/com/example/voicebreakah/Settings.java
@@ -2,7 +2,10 @@ package com.example.voicebreakah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ImageView backArrow = findViewById(R.id.settings_back_arrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
