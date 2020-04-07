f54003327803a7e7ebcf58b5951f07482f4e5a18
Stanley Zheng <szheng22@jhu.edu>
2020-04-07 00:38:35 -0400

    added buttons

--- app/src/main/java/com/example/voicebreakah/MainActivity.java
+++ app/src/main/java/com/example/voicebreakah/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
